70c9e2b3e1a46eced9cb09f6d85a3fe3d520aecd
octobaking <baptistawachira41@gmail.com>
2026-06-09 06:15:47 +0300

    Update API_TOKEN in bot.js

--- bot.js
+++ bot.js
@@ -54,7 +54,7 @@ const path = require('path');
 // ========================== CONFIGURATION ==========================
 const CONFIG = {
     APP_ID: '1089',
-    API_TOKEN: '', be50afb504b3ad44',// DEMO token
+    API_TOKEN: 'be50afb504b3ad44',// DEMO token
     TELEGRAM: { TOKEN: '8428234507:AAG4RkuOnz4BNK0i2iFtW2U9TfCVf1p7T8', CHAT_ID: '83274404422' },
     STAKE: 1,                         // Base stake ($1), but volatility targeting may adjust it
     MAX_CONCURRENT: 25,
